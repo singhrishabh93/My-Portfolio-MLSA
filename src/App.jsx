94b9110555f62a3884525951c4238fbd0b25e51a
Rishabh Singh <singhrishabh1670@gmail.com>
2023-06-19 05:42:19 +0000

    Technical Onboarding MLSA

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Rishabh Singh",
+  title: "Alpha MLSA, SDE Intern @ NoQs Digital Private Limited & Google Cloud Digital Leader",
+  email: "rishabh.singh1@studentambassadors.com",
+  gitHub: "singhrishabh93",
+  instagram: "singhrishabh1670",
+  linkedIn: "singhrishabh1670",
+  medium: "singhrishabh1670",
+  twitter: "singhrishabh93",
+  youTube: "carified",
 };
 
 const primaryColor = "#4E567E";
